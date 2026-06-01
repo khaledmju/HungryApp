d304f6b3f1f413599d78ea0e6e168e175626efe8
khaledmju <khaledjoouu@gmail.com>
2026-06-01 11:11:44 +0300

    project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hungry/views/home_page.dart';
+import 'package:hungry/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Hungry', home: HomePage());
+    return MaterialApp(
+      title: 'Hungry',
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
